e48a24227b2755727e7c48caa2469bdbf3d1420f
Juan Picado <juan@encuestame.org>
2009-05-13 23:06:34 +0000

    comments
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@36 39e2479b-dd58-4041-9f67-69c9b71f0610

--- src/org/jp/web/beans/ServiceManagerBean.java
+++ src/org/jp/web/beans/ServiceManagerBean.java
@@ -11,22 +11,27 @@ import org.jp.web.beans.commons.MessageSourceFactoryBean;
 import org.jp.web.serviceManager.ServiceManager;
 
 /**
- * encuestame: system online surveys Copyright (C) 2008-2009 encuestame
- * Development Team * This program is free software; you can redistribute it
- * and/or modify it under the terms of version 3 of the GNU General Public
- * License as published by the Free Software Foundation. * This program is
- * distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details. *
+ * encuestame: system online surveys Copyright (C) 2005-2008 encuestame
+ * Development Team
+ * 
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA * Id: ServiceLocateBean.java 1822
- * 26/04/2009 20:25:26
+ * Place, Suite 330, Boston, MA 02111-1307 USA
  * 
- * @author dianmorales
- * @version 1.0 package org.jp.web.beans
+ * Id: ServiceManagerBean.java Date: 26/04/2009
+ * 
+ * @author dianmorales package: org.jp.web.beans
+ * @version 1.0
  */
-
 public class ServiceManagerBean implements ServiceManager {
 
 	private DataService dataService;
