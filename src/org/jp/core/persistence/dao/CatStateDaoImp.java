02e6babd836e855178a45d2d7b5554862ba206b2
Juan Picado <juan@encuestame.org>
2009-06-26 00:01:42 +0000

    git-svn-id: http://svn.encuesta.me/trunk/encuestame@93 39e2479b-dd58-4041-9f67-69c9b71f0610

--- src/org/jp/core/persistence/dao/CatStateDaoImp.java
+++ src/org/jp/core/persistence/dao/CatStateDaoImp.java
@@ -24,7 +24,7 @@ import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
  * details.
  * 
  * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * this program; if not, writes to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
  * 
  * Id: CatStateDaoImp.java Date: 26/04/2009
