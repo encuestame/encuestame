3d2316e707b732b44eaca05003f981d6b352541e
Juan Picado <juan@encuestame.org>
2009-06-26 00:19:58 +0000

    test2
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@96 39e2479b-dd58-4041-9f67-69c9b71f0610

--- src/org/jp/core/mail/MailService.java
+++ src/org/jp/core/mail/MailService.java
@@ -7,7 +7,7 @@ import org.springframework.mail.MailSendException;
  * Team
  * 
  * This program is free software; you can redistribute it and/or modify it under
- * the terms of version 3 of the GNU General Public License as published by the
+ * the terms of vesrsion 3 of the GNU General Public License as published by the
  * Free Software Foundation.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT
