c53e6fc2f7237583bc968e51636f3e42788dd3b9
Juan Picado <juan@encuestame.org>
2009-10-17 03:17:57 +0000

    Fixed checkstyle and pmd bug.
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@141 39e2479b-dd58-4041-9f67-69c9b71f0610

--- source/java/org/encuestame/core/exception/EnMeExpcetion.java
+++ source/java/org/encuestame/core/exception/EnMeExpcetion.java
@@ -1,52 +1,62 @@
-package org.encuestame.core.exception;
 /**
- * encuestame:  system online surveys
- * Copyright (C) 2009 encuestame Development Team
+ * encuestame: system online surveys Copyright (C) 2005-2008 encuestame
+ * Development Team
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of version 3 of the GNU General Public
- * License as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published by the
+ * Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA
- *
- * Id: EnMeExpcetion.java Date: 07/05/2009 
- * @author juanpicado
- * package: org.encuestame.core.exception 
- * @version 1.0
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, writes to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+package org.encuestame.core.exception;
+ /**
+ * Encuestame Exception.
+ * @author Picado, Juan juan@encuestame.org
+ * @since May 07, 2009
  */
 public class EnMeExpcetion extends Exception{
 
-	
-	private static final long serialVersionUID = 7631058192250904935L;
+    /** serial. */
+    private static final long serialVersionUID = 7631058192250904935L;
 
-	public EnMeExpcetion() {
-		super();
-		
-	}
+    /**
+     * Constructor.
+     */
+    public EnMeExpcetion() {
+        super();
 
-	public EnMeExpcetion(String message, Throwable cause) {
-		super(message, cause);
-	
-	}
+    }
+    /**
+     * Exception.
+     * @param message message
+     * @param cause cause
+     */
+    public EnMeExpcetion(final String message, final Throwable cause) {
+        super(message, cause);
 
-	public EnMeExpcetion(String message) {
-		super(message);
-		
-	}
+    }
+    /**
+     * Exception.
+     * @param message message
+     */
+    public EnMeExpcetion(final String message) {
+        super(message);
 
-	public EnMeExpcetion(Throwable cause) {
-		super(cause);
-		
-	}
-	
-	
+    }
 
+    /**
+     * Exception.
+     * @param cause cause
+     */
+    public EnMeExpcetion(final Throwable cause) {
+        super(cause);
+
+    }
 }
