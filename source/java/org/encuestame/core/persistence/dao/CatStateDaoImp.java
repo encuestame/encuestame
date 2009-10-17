62b587fb432c7382b1ee98f402946dd4677e7c6d
Juan Picado <juan@encuestame.org>
2009-10-17 02:52:18 +0000

    Fixed checkstyle and pmd bug.
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@140 39e2479b-dd58-4041-9f67-69c9b71f0610

--- source/java/org/encuestame/core/persistence/dao/CatStateDaoImp.java
+++ source/java/org/encuestame/core/persistence/dao/CatStateDaoImp.java
@@ -1,75 +1,70 @@
+/**
+ * encuestame: system online surveys Copyright (C) 2005-2008 encuestame
+ * Development Team
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, writes to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 package org.encuestame.core.persistence.dao;
 
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.HibernateException;
 import org.encuestame.core.persistence.dao.imp.ICatState;
 import org.encuestame.core.persistence.pojo.CatState;
-import org.encuestame.core.persistence.pojo.SecUsers;
+import org.hibernate.HibernateException;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 /**
- * encuestame: system online surveys Copyright (C) 2005-2008 encuestame
- * Development Team
- * 
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of version 3 of the GNU General Public License as published by the
- * Free Software Foundation.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- * 
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, writes to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- * 
- * Id: CatStateDaoImp.java Date: 26/04/2009
- * 
- * @author juanpicado package: org.encuestame.core.persistence.dao
- * @version 1.0
+ * Catalog State Dao.
+ * @author Picado, Juan juan@encuestame.org
+ * @since April 26, 2009
  */
 public class CatStateDaoImp extends HibernateDaoSupport implements ICatState {
 
-	private Log log = LogFactory.getLog(this.getClass());
+    private Log log = LogFactory.getLog(this.getClass());
 
-	
-	/**
-	 * delete state
-	 * @param state
-	 * @throws HibernateException
-	 */
-	public void delete(CatState state) throws HibernateException{
-		getHibernateTemplate().delete(state);
-	}
 
-	/**
-	 * load all states
-	 * 
-	 * @return
-	 */
-	public List<CatState> findAll() throws HibernateException {
-		return getHibernateTemplate().find("from CatState");
-	}
+    /**
+     * Delete catalog state.
+     * @param state catalog state
+     * @throws HibernateException exception
+     */
+    public void delete(final CatState state) throws HibernateException{
+        getHibernateTemplate().delete(state);
+    }
 
-	/**
-	 * get state
-	 * 
-	 * @param id
-	 * @return
-	 */
-	public CatState getState(Integer id) throws HibernateException {
-		List<CatState> state = getHibernateTemplate().findByNamedQuery(
-				"State.loadStateByUsername", id);
-		// obtiene el primer elemento
-		if (state.size() > 0) {
-			return (CatState) state.get(0);
-		} else {
-			return null;
-		}
-	}
+    /**
+     * Retrieve all states.
+     * @return list of states
+     */
+    public List<CatState> findAll() throws HibernateException {
+        return getHibernateTemplate().find("from CatState");
+    }
 
+    /**
+     * Get catalog state by id.
+     * @param id id state
+     * @return state
+     */
+    public CatState getState(final Integer id) throws HibernateException {
+        List<CatState> state = getHibernateTemplate().findByNamedQuery(
+                "State.loadStateByUsername", id);
+        if (state.size() > 0) {
+            return (CatState) state.get(0);
+        } else {
+            return null;
+        }
+    }
 }
