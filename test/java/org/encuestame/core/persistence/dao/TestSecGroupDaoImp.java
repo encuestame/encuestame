fa00e2552b28331b602497cc3aa40641492af0c8
Paola Morales <paola@encuestame.org>
2009-10-31 06:45:27 +0000

    adding method to TestCase
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@185 39e2479b-dd58-4041-9f67-69c9b71f0610

--- test/java/org/encuestame/core/persistence/dao/TestSecGroupDaoImp.java
+++ test/java/org/encuestame/core/persistence/dao/TestSecGroupDaoImp.java
@@ -1,14 +1,34 @@
 /**
+ * encuestame: system online surveys Copyright (C) 2009 encuestame Development
+ * Team
  *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of version 3 of the GNU General Public License as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package org.encuestame.core.persistence.dao;
 
+import com.puppycrawl.tools.checkstyle.checks.coding.SuperCloneCheck;
+
 import org.encuestame.test.config.AbstractBaseTest;
+import org.hibernate.id.IdentityGenerator.GetGeneratedKeysDelegate;
 import org.junit.Test;
 import org.encuestame.core.persistence.pojo.SecGroups;
+import org.encuestame.core.persistence.pojo.SecUsers;
 /**
- * @author paola
+ * UserDao.
  *
+ * @author Morales, Diana Paola paola@encuestame.org
+ * @since October 29, 2009
  */
 public class TestSecGroupDaoImp extends AbstractBaseTest{
 
@@ -17,7 +37,7 @@ public class TestSecGroupDaoImp extends AbstractBaseTest{
  */
     @Test
     public void TestCreateGroup(){
-        final SecGroups group = super.createGroups("grupo uno");
+        final SecGroups group = super.createGroups("first group");
         assertNotNull(group);
     }
 
@@ -26,9 +46,36 @@ public class TestSecGroupDaoImp extends AbstractBaseTest{
      */
     @Test
    public void TestDeleteGroup(){
-        final SecGroups group = super.createGroups("grupo dos");
+        final SecGroups group = super.createGroups("second group");
         getSecGroup().Delete(group);
         assertEquals("Should be equals",0, getSecGroup().findAllGroups().size());
+    }
+
+    /**Test Find All Groups*/
+    @Test
+    public void TestFindGroup()
+    {
+        super.createGroups("group thirth");
+        super.createGroups("group fourth");
+        assertEquals("Should be equals",2, getSecGroup().findAllGroups().size());
+
+    }
+    /**Test Update Group**/
+    @Test
+    public void TestUpdateGroup(){
+    final String newname="Administrator";
+    final String newdescription="System Admin";
+    final SecGroups group = super.createGroups("fifth group");
+    group.setName(newname);
+    group.setDesInfo(newdescription);
+    getSecGroup().update(group);
+    final SecGroups retrieveGroup = getSecGroup().getGroupById(Long.valueOf(
+          group.getGroupId().toString()));
+    assertEquals("New Name should be",newname,
+            retrieveGroup.getName());
+    assertEquals("New Description should be",newdescription,
+            retrieveGroup.getDesInfo());
+
     }
 
 }
