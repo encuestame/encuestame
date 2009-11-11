f32bfe8d50c2ff142d385231f64af2e7a60ff849
Paola Morales <paola@encuestame.org>
2009-11-11 02:09:11 +0000

    New SurveyFormat Dao
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@211 39e2479b-dd58-4041-9f67-69c9b71f0610

---
+++ source/java/org/encuestame/core/persistence/dao/imp/ISurveyFormatDao.java
@@ -0,0 +1,30 @@
+/**
+ * encuestame:  system online surveys
+ * Copyright (C) 2005-2008 encuestame Development Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+package org.encuestame.core.persistence.dao.imp;
+
+
+/**
+ * SurveyFormat Interface.
+ *
+ * @author Morales Urbina, Diana paola@encuestame.org
+ * @since November 10, 2009
+ */
+public interface ISurveyFormatDao extends IBaseDao {
+
+}
