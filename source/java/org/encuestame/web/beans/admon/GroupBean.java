c03e7aab917c3a718735730e2891689ad5d07997
Juan Picado <juan@encuestame.org>
2009-12-01 03:12:30 +0000

    Javadoc and fix warnings on group bean.
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@268 39e2479b-dd58-4041-9f67-69c9b71f0610

--- source/java/org/encuestame/web/beans/admon/GroupBean.java
+++ source/java/org/encuestame/web/beans/admon/GroupBean.java
@@ -1,73 +1,80 @@
-package org.encuestame.web.beans.admon;
 /**
- * encuestame: system online surveys Copyright (C) 2005-2008 encuestame
- * Development Team
+ * encuestame:  system online surveys
+ * Copyright (C) 2005-2008 encuestame Development Team
  *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of version 3 of the GNU General Public License as published by the
- * Free Software Foundation.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of version 3 of the GNU General Public
+ * License as published by the Free Software Foundation.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with
- * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- *
- * Id: GroupBean.java Date: 27/04/2009
- *
- * @author juanpicado package: org.encuestame.web.beans.admon
- * @version 1.0
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
  */
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
+package org.encuestame.web.beans.admon;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import java.util.LinkedList;
+import java.util.List;
+
 import org.encuestame.core.persistence.pojo.SecGroups;
 import org.encuestame.web.beans.MasterBean;
 
+/**
+ * Group Administrator Bean.
+ * @author Picado, Juan juan@encuestame.org
+ * @since 27/04/2009
+ * @version $Id
+ */
+
 public class GroupBean extends MasterBean {
 
+    /** New {@link UnitGroupBean} **/
     private UnitGroupBean newGroup;
-    private Collection<UnitGroupBean> list_unitBeans;
-    private Log log = LogFactory.getLog(this.getClass());
-
+    /** List of {@link UnitGroupBean} **/
+    private List<UnitGroupBean> listUnitGroupBeans;
+    /** Processed Group Id **/
     private Integer processedGroupId;
 
+    /**
+     * Constructor.
+     */
     public GroupBean() {
         isOneRow = true;
     }
 
-    private Collection<UnitGroupBean> getLoadListGroups() {
+    /**
+     * Load list of Groups.
+     * @return List of {@link UnitGroupBean}
+     */
+    private List<UnitGroupBean> getLoadListGroups() {
+        listUnitGroupBeans = new LinkedList<UnitGroupBean>();
         try {
-            list_unitBeans = new LinkedList<UnitGroupBean>();
-            Collection<SecGroups> listGroups = getServicemanager().getApplicationServices()
+            final List<SecGroups> listGroups = getServicemanager().getApplicationServices()
                     .getSecurityService().getGroupDao().findAllGroups();
-            for (Iterator<SecGroups> i = listGroups.iterator(); i.hasNext();) {
-                UnitGroupBean newGroup = new UnitGroupBean();
-                SecGroups group = i.next();
+            for (SecGroups group : listGroups) {
+                final UnitGroupBean newGroup = new UnitGroupBean();
                 newGroup.setId(Integer.valueOf(group.getGroupId().toString()));
                 newGroup.setGroupName(group.getName());
                 newGroup.setGroupDescription(group.getDesInfo());
                 newGroup.setStateId((group.getIdState().toString()));
-                list_unitBeans.add(newGroup);
+                listUnitGroupBeans.add(newGroup);
             }
-            return list_unitBeans;
         } catch (Exception e) {
             addErrorMessage(getMessageProperties("error_load_groups"), e
                     .getMessage());
             log.error("error load groups");
-            return list_unitBeans = new LinkedList<UnitGroupBean>();
         }
-
+        return listUnitGroupBeans;
     }
 
+
     /**
-     * new group
+     * Create a Group.
      */
     public void newGroup() {
         try {
@@ -84,6 +91,9 @@ public class GroupBean extends MasterBean {
         }
     }
 
+    /**
+     * Update Group.
+     */
     public void updateGroup() {
         try {
             log.info("new group");
@@ -94,13 +104,13 @@ public class GroupBean extends MasterBean {
         } catch (Exception e) {
             addErrorMessage(getMessageProperties("errorCreateNewGroup"), e
                     .getMessage());
-            log.error("error new group->"
+            log.error("error new group: "
                     + getMessageProperties("errorCreateNewGroup"));
         }
     }
 
     /**
-     * Delete Group
+     * Delete Group.
      */
     public void deleteGroup() {
         try {
@@ -109,7 +119,6 @@ public class GroupBean extends MasterBean {
                 getServicemanager().getApplicationServices().getSecurityService().deleteGroup(
                         getNewGroup());
                 reset();
-
             } else {
                 addErrorMessage(getMessageProperties("errorDelteGroup"),
                         "errorDelteGroup");
@@ -121,6 +130,9 @@ public class GroupBean extends MasterBean {
         }
     }
 
+    /**
+     * Reset Bean.
+     */
     public void reset() {
         newGroup.setId(null);
         newGroup.setGroupDescription(null);
@@ -129,64 +141,81 @@ public class GroupBean extends MasterBean {
     }
 
     /**
-     *
-     * @param newGroup
+     * Fill Table.
+     * @param idGroup groupd id
      */
-    @Deprecated
-    private void reset(UnitGroupBean newGroup) {
-        newGroup.setId(null);
-        newGroup.setGroupDescription(null);
-        newGroup.setGroupName(null);
-        newGroup.setStateId(null);
-    }
-
     private void fill(Integer idGroup) {
-        SecGroups s = getServicemanager().getApplicationServices().getSecurityService()
-                .getGroupDao().find(Long.valueOf(idGroup));
         try {
-            if (s != null) {
+        final SecGroups secGroups = getServicemanager().getApplicationServices().getSecurityService()
+                .getGroupDao().find(Long.valueOf(idGroup));
+            if (secGroups != null) {
                 reset();
-                this.newGroup.setId(Integer.valueOf(s.getGroupId().toString()));
-                this.newGroup.setGroupDescription(s.getDesInfo());
-                this.newGroup.setGroupName(s.getName());
-                this.newGroup.setStateId((s.getIdState().toString()));
+                this.newGroup.setId(Integer.valueOf(secGroups.getGroupId().toString()));
+                this.newGroup.setGroupDescription(secGroups.getDesInfo());
+                this.newGroup.setGroupName(secGroups.getName());
+                this.newGroup.setStateId((secGroups.getIdState().toString()));
             } else {
-                addErrorMessage("No se pudo recuperar Grupo", "");
+                addErrorMessage("group can't be created", "");
             }
         } catch (Exception e) {
-            addErrorMessage("No se pudo recuperar Grupo" + e.getMessage(), e
+            addErrorMessage("error on retrieve list of groups: " + e.getMessage(), e
                     .getMessage());
         }
     }
 
+    /**
+     * Getter.
+     * @return {@link UnitGroupBean}
+     */
     public UnitGroupBean getNewGroup() {
         return newGroup;
     }
 
+    /**
+     * Setter.
+     * @param newGroup new group
+     */
     public void setNewGroup(UnitGroupBean newGroup) {
         this.newGroup = newGroup;
     }
 
-    public Collection<UnitGroupBean> getList_unitBeans() {
+    /**
+     * Getter.
+     * @return list of {@link UnitGroupBean}
+     */
+    public List<UnitGroupBean> getListUnitGroupBeans() {
         getLoadListGroups();
-        if (list_unitBeans.size() > 0)
+        if (listUnitGroupBeans.size() > 0) {
             isOneRow = true;
-        else
+        } else {
             isOneRow = false;
-        return list_unitBeans;
+        }
+        return listUnitGroupBeans;
     }
 
-    public void setList_unitBeans(Collection<UnitGroupBean> list_unitBeans) {
-        this.list_unitBeans = list_unitBeans;
+    /**
+     * Setter.
+     * @param listUnitBeans list of {@link UnitGroupBean}
+     */
+    public void setListUnitGroupBeans(List<UnitGroupBean> listUnitBeans) {
+        this.listUnitGroupBeans = listUnitBeans;
     }
 
 
 
+    /**
+     * Setter.
+     * @return processed group id
+     */
     public Integer getProcessedGroupId() {
         log.info("getProcessedGroupId " + processedGroupId);
         return processedGroupId;
     }
 
+    /**
+     * Setter.
+     * @param processedGroupId processed group id
+     */
     public void setProcessedGroupId(Integer processedGroupId) {
         try {
             log.info("setProcessedGroupId " + processedGroupId);
