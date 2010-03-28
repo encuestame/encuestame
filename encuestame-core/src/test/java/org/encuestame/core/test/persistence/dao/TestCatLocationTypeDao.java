267358cccf95bbcbc685f856debe4cf6c1d74f4d
Juan Picado <juan@encuestame.org>
2010-03-28 15:04:43 +0000

    Merge with juanpicado branch
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@447 39e2479b-dd58-4041-9f67-69c9b71f0610

---
+++ encuestame-core/src/test/java/org/encuestame/core/test/persistence/dao/TestCatLocationTypeDao.java
@@ -0,0 +1,50 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2001-2009 encuestame: system online surveys Copyright (C) 2009
+ * encuestame Development Team.
+ * Licensed under the Apache Software License version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to  in writing,  software  distributed
+ * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+ * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+ * specific language governing permissions and limitations under the License.
+ ************************************************************************************
+ */
+package org.encuestame.core.test.persistence.dao;
+
+import org.encuestame.core.persistence.pojo.CatLocationType;
+import org.encuestame.core.test.service.config.AbstractBase;
+import org.junit.Test;
+
+/**
+ * Test Catatalog Location Type Dao.
+ * @author Morales, Diana Paola paola@encuestame.org
+ * @since November 17,2009 09:10:55
+ * @version $Id$
+ */
+public class TestCatLocationTypeDao extends AbstractBase{
+
+    /**
+     * Test List Location Type.
+     */
+    @Test
+    public void testListLocationType(){
+       // super.createCatLocationType("First Location Type");
+        // final List<CatLocationType> listCatLocationType = getCatLocationType().findAll();
+        //    assertNotNull(listCatLocationType);
+        //    assertEquals(listCatLocationType.size(),listCatLocationType.size());
+    }
+
+    /**
+     * Test Location Type by Id.
+     */
+    @Test
+    public void testLocationTypeByID(){
+     //   CatLocationType locType = super.createCatLocationType("Second Location Type");
+        //final CatLocationType retrieveLocType = getCatLocationType().getLocationTypeById(Long.valueOf(locType.getLocationTypeId().toString()));
+        //assertNotNull(retrieveLocType);
+
+    }
+
+
+}
---
+++ encuestame-core/src/test/java/org/encuestame/core/test/persistence/pojos/TestSurveyFormat.java
@@ -0,0 +1,40 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2001-2009 encuestame: system online surveys Copyright (C) 2009
+ * encuestame Development Team.
+ * Licensed under the Apache Software License version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to  in writing,  software  distributed
+ * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+ * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+ * specific language governing permissions and limitations under the License.
+ ************************************************************************************
+ */
+package org.encuestame.core.test.persistence.pojos;
+
+import org.encuestame.core.persistence.pojo.SurveyFormat;
+import org.encuestame.core.test.service.config.AbstractBase;
+import org.junit.Test;
+
+/**
+* Test Survey Format Pojo.
+* @author Morales, Diana Paola paola@encuestame.org
+* @since November 14, 2009
+* @version $Id$
+*/
+
+public class TestSurveyFormat extends AbstractBase{
+
+/**
+ * Test Survey Format.
+ */
+@Test
+    public void testSurveyFormat(){
+    SurveyFormat surveyformat = new SurveyFormat();
+    surveyformat.setDateCreated(null);
+    surveyformat.setSurveyFormatName("Education");
+    getSurveyFormatDaoImp().saveOrUpdate(surveyformat);
+
+    }
+
+}
