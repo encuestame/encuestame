fdbfe9a08f18418d8e8677415d42c459d684e4b2
Juan Picado <juan@encuestame.org>
2010-03-28 03:41:12 +0000

    Merge from juanpicado branche
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@444 39e2479b-dd58-4041-9f67-69c9b71f0610

---
+++ encuestame-core/src/test/java/org/encuestame/core/persistence/dao/TestClientDao.java
@@ -0,0 +1,64 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2001-2010 encuestame: system online surveys Copyright (C) 2010
+ * encuestame Development Team.
+ * Licensed under the Apache Software License version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to  in writing,  software  distributed
+ * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+ * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+ * specific language governing permissions and limitations under the License.
+ ************************************************************************************
+ */
+
+package org.encuestame.core.persistence.dao;
+
+import java.util.List;
+
+import org.encuestame.core.persistence.pojo.Client;
+import org.encuestame.core.persistence.pojo.Project;
+import org.encuestame.core.service.config.AbstractBase;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * {@link ClientDao} test.
+ * @author Picado, Juan juan@encuestame.org
+ * @since Jan 24, 2010 5:12:28 PM
+ * @version $Id: change to one dolar simbol
+ */
+public class TestClientDao  extends AbstractBase{
+
+    /** {@link Project}. */
+    private Project project;
+
+    /**
+     * Before.
+     */
+    @Before
+    public void initBefore(){
+        project = createProject("project1", "description", "info", createState("active"), createUser());
+    }
+
+    /**
+     * Get Client.
+     */
+    @Test
+    public void testGetClientById(){
+         final Client client = createClient("client1", project);
+         final Client clientRetrieved = getClientDao().getClientById(client.getClientId());
+         assertEquals("Should be", client.getClientId(), clientRetrieved.getClientId());
+    }
+
+    /**
+     * Test find clients by project.
+     */
+    @Test
+    public void testFindAllClientByProjectId(){
+        createClient("client1", project);
+        createClient("client2", project);
+        createClient("client3", project);
+        final List<Client> listofClients = getClientDao().findAllClientByProjectId(project.getProyectId());
+        assertEquals("Should be", 3, listofClients.size());
+    }
+}
---
+++ encuestame-core/src/test/java/org/encuestame/core/persistence/dao/TestUserDao.java
@@ -0,0 +1,140 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2001-2009 encuestame: system online surveys Copyright (C) 2009
+ * encuestame Development Team.
+ * Licensed under the Apache Software License version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to  in writing,  software  distributed
+ * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+ * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+ * specific language governing permissions and limitations under the License.
+ ************************************************************************************
+ */
+package org.encuestame.core.persistence.dao;
+
+import java.util.List;
+
+import org.encuestame.core.persistence.pojo.SecGroups;
+import org.encuestame.core.persistence.pojo.SecPermission;
+import org.encuestame.core.persistence.pojo.SecUserSecondary;
+import org.encuestame.core.persistence.pojo.SecUsers;
+import org.encuestame.core.service.config.AbstractBase;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * UserDao.
+ * @author Morales, Diana Paola paola@encuestame.org
+ * @since October 27, 2009
+ * @version $Id$
+ */
+public class TestUserDao extends AbstractBase {
+
+    private SecUsers userPrimary;
+
+    /**
+     * Before.
+     **/
+    @Before
+    public void initService(){
+        this.userPrimary = createUser();
+    }
+
+    /***
+     *Test Create User.
+     */
+    @Test
+    public void testCreateUser() {
+       final SecUserSecondary user = createSecondaryUser("user 1", this.userPrimary);
+       assertNotNull(user);
+    }
+
+    /**
+     * Test delete user method.
+     **/
+    @Test
+    public void testDeleteUser() {
+        final SecUserSecondary user = createSecondaryUser("user 2", this.userPrimary);
+         getSecUserDao().delete(user);
+        assertEquals("Should be equals",0, getSecUserDao().findAll().size());
+    }
+
+    /**
+     * Test find all users method.
+     */
+    @Test
+    public void testFindAllUsers() {
+        createSecondaryUser("user 1", this.userPrimary);
+        createSecondaryUser("user 2", this.userPrimary);
+        assertEquals("Should be equals",2, getSecUserDao().findAll().size());
+    }
+
+    /**
+     * Test Update user.
+     **/
+    @Test
+    public void testUpdateUser(){
+        final String newPassword = "67809";
+        final String newEmail = "user2@users.com";
+        final SecUserSecondary user = createSecondaryUser("user 1", this.userPrimary);
+        user.setPassword(newPassword);
+        user.setUserEmail(newEmail);
+        getSecUserDao().saveOrUpdate(user);
+         final SecUserSecondary retrieveUser = getSecUserDao()
+         .getSecondaryUserById(Long.valueOf(user.getUid()));
+     assertEquals("Password should be",newPassword,
+                     retrieveUser.getPassword());
+        assertEquals("Email should be",newEmail,
+               retrieveUser.getUserEmail());
+    }
+
+    /**
+     * Test Get User by Username.
+     **/
+    @Test
+    public void testGetUserByUsername(){
+        final SecUserSecondary user = createSecondaryUser("user 3", this.userPrimary);
+        final SecUserSecondary retrieveUser = getSecUserDao()
+        .getUserByUsername(user.getUsername());
+        assertEquals("Username should be",user.getUsername(), retrieveUser.getUsername());
+    }
+
+    /**
+     * Test Assing Group to User.
+     **/
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testAssingGroupToUser(){
+         final SecUserSecondary user = createSecondaryUser("user 4", this.userPrimary);
+         final SecGroups group = super.createGroups("group 1");
+         user.getSecGroups().add(group);
+         getSecGroup().saveOrUpdate(user);
+         assertEquals("Should be equals", 1, user.getSecGroups().size());
+    }
+
+    /**
+     * Test Add Permission to Group.
+     */
+    @Test
+    public void testAddPermissionToGroup(){
+        final SecPermission editor = createPermission("editor");
+        final SecPermission admon = createPermission("publisher");
+        final SecPermission permission = createPermission("administrator");
+        final SecGroups group = createGroups("group 1");
+        group.getSecPermissions().add(editor);
+        group.getSecPermissions().add(admon);
+        group.getSecPermissions().add(permission);
+        getSecGroup().saveOrUpdate(group);
+        assertEquals("Should be equals", 3, group.getSecPermissions().size());
+    }
+
+    /**
+     * Test getSecondaryUsersByUserId.
+     */
+    public void testGetSecondaryUsersByUserId(){
+         createSecondaryUser("user 1", this.userPrimary);
+         createSecondaryUser("user 2", this.userPrimary);
+         final List<SecUserSecondary> userList = getSecUserDao().getSecondaryUsersByUserId(this.userPrimary.getUid());
+         assertEquals("Should be equals", 2, userList.size());
+    }
+}
