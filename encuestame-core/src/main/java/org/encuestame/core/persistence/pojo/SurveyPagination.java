5dd0c6e27683e3db3eb98854ab819dfa0a170fe7
Paola Morales <paola@encuestame.org>
2010-08-17 03:04:24 +0000

    New Survey's Pojo.  * Pagination and Folder Survey's Pojo   * Dependencies in questions or exclusion Login in questions
    git-svn-id: http://svn.encuesta.me/trunk/encuestame@716 39e2479b-dd58-4041-9f67-69c9b71f0610

---
+++ encuestame-core/src/main/java/org/encuestame/core/persistence/pojo/SurveyPagination.java
@@ -0,0 +1,132 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2001-2009 encuestame: system online surveys Copyright (C) 2009
+ * encuestame Development Team.
+ * Licensed under the Apache Software License version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to  in writing,  software  distributed
+ * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+ * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+ * specific language governing permissions and limitations under the License.
+ ************************************************************************************
+ */
+package org.encuestame.core.persistence.pojo;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Survey Pagination.
+ * @author Morales Urbina, Diana paola@encuestame.org
+ * @since August 10, 2010
+ * @version $Id: $
+ */
+
+@Entity
+@Table(name = "survey_pagination")
+public class SurveyPagination {
+
+    /****/
+    private Long paginationId;
+
+    /****/
+    private Surveys surveys;
+
+    /****/
+    private Question questions;
+
+    /****/
+    private Long pageNumber;
+
+    /****/
+    private QuestionsAnswers answers;
+
+    /**
+     * @return the paginationId
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "pagination_id", unique = true, nullable = false)
+    public Long getPaginationId() {
+        return paginationId;
+    }
+
+    /**
+     * @param paginationId the paginationId to set
+     */
+    public void setPaginationId(Long paginationId) {
+        this.paginationId = paginationId;
+    }
+
+    /**
+     * @return the surveys
+     */
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "sid", nullable = false)
+    public Surveys getSurveys() {
+        return surveys;
+    }
+
+    /**
+     * @param surveys the surveys to set
+     */
+    public void setSurveys(Surveys surveys) {
+        this.surveys = surveys;
+    }
+
+    /**
+     * @return the questions
+     */
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "id_question", nullable = false)
+    public Question getQuestions() {
+        return questions;
+    }
+
+    /**
+     * @param questions the questions to set
+     */
+    public void setQuestions(Question questions) {
+        this.questions = questions;
+    }
+
+    /**
+     * @return the pageNumber
+     */
+    @Column(name = "created_at", length = 0)
+    public Long getPageNumber() {
+        return pageNumber;
+    }
+
+    /**
+     * @param pageNumber the pageNumber to set
+     */
+    public void setPageNumber(Long pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    /**
+     * @return the answers
+     */
+    @ManyToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "q_answer_id", nullable = false)
+    public QuestionsAnswers getAnswers() {
+        return answers;
+    }
+
+    /**
+     * @param answers the answers to set
+     */
+    public void setAnswers(QuestionsAnswers answers) {
+        this.answers = answers;
+    }
+
+
+ }
