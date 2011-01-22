cbad159de3911c94035328f51b4aa7663d2983b7
Juan Picado <juan@encuestame.org>
2011-01-22 10:14:41 -0600

    SignUp validator

---
+++ encuestame-mvc/src/main/java/org/encuestame/mvc/validator/SignUpBeanValidator.java
@@ -0,0 +1,81 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2001-2011 encuestame: system online surveys Copyright (C) 2009
+ * encuestame Development Team.
+ * Licensed under the Apache Software License version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to  in writing,  software  distributed
+ * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+ * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+ * specific language governing permissions and limitations under the License.
+ ************************************************************************************
+ */
+package org.encuestame.mvc.validator;
+
+import org.apache.log4j.Logger;
+import org.encuestame.business.service.SecurityService;
+import org.encuestame.business.service.imp.ISecurityService;
+import org.encuestame.mvc.controller.validation.ControllerValidation;
+import org.encuestame.utils.security.SignUpBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.binding.message.MessageBuilder;
+import org.springframework.binding.message.MessageContext;
+import org.springframework.binding.validation.ValidationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * Validator for SignUp.
+ * @author Picado, Juan juanATencuestame.org
+ * @since Jan 22, 2011 9:09:11 AM
+ * @version $Id:$
+ */
+@Component
+public class SignUpBeanValidator{
+
+    /**
+     * Log.
+     */
+    protected Logger log = Logger.getLogger(this.getClass());
+
+    /**
+     * Reference to {@link SecurityService}.
+     */
+    @Autowired
+    private ISecurityService securityService;
+
+    /**
+     * Validate SignUn Bean.
+     * @param booking
+     * @param context
+     */
+    public void validate(SignUpBean booking, ValidationContext context) {
+        final ControllerValidation validation = new ControllerValidation(securityService);
+        MessageContext messages = context.getMessageContext();
+        if(validation.validateUserByEmail(booking.getEmail()) != null){
+            log.warn("Email NOT VALID");
+            //result.rejectValue("email", "secure.email.notvalid"); //secure.email.notvalid
+            messages.addMessage(new MessageBuilder().error().source("email").
+                    defaultText("Email NOT VALID").build());
+        }
+        if(!validation.validateUsername(booking.getUsername())){
+            log.warn("Username NOT VALID");
+             //result.rejectValue("username", "secure.user.notvalid"); //secure.user.notvalid
+            messages.addMessage(new MessageBuilder().error().source("username").
+                    defaultText("Username NOT VALID").build());
+        }
+    }
+
+    /**
+     * @return the securityService
+     */
+    public ISecurityService getSecurityService() {
+        return securityService;
+    }
+
+    /**
+     * @param securityService the securityService to set
+     */
+    public void setSecurityService(final ISecurityService securityService) {
+        this.securityService = securityService;
+    }
+}
