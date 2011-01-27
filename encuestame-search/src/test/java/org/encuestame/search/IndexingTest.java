b70e06c0aaeb65602ae3ded0d9e6d1dfd6790a45
Paola Morales <paola@encuestame.org>
2011-01-27 09:53:52 -0600

    Testing Delete, Update, Add methods in Lucene

--- encuestame-search/src/test/java/org/encuestame/search/IndexingTest.java
+++ encuestame-search/src/test/java/org/encuestame/search/IndexingTest.java
@@ -1,6 +1,7 @@
 package org.encuestame.search;
 
 import java.io.IOException;
+import java.io.PrintStream;
 
 import org.apache.lucene.analysis.WhitespaceAnalyzer;
 import org.apache.lucene.document.Document;
@@ -19,9 +20,9 @@ import org.junit.Ignore;
 import com.mchange.v2.c3p0.util.TestUtils;
 
 import static org.junit.Assert.assertEquals;
-
+import junit.framework.TestCase;
 @Ignore
-public class IndexingTest {
+public class IndexingTest extends TestCase{
 	protected String[] ids = { "1", "2" };
 	protected String[] unindexed = { "Netherlands", "Italy" };
 	protected String[] unstored = { "Amsterdam has lots of bridges",
@@ -50,6 +51,7 @@ public class IndexingTest {
 	}
 
 	private IndexWriter getWriter() throws IOException {
+		System.out.println("************************");
 		return new IndexWriter(directory, new WhitespaceAnalyzer(),
 				IndexWriter.MaxFieldLength.UNLIMITED);
 	}
@@ -74,8 +76,46 @@ public class IndexingTest {
 
 	public void testIndexReader() throws IOException {
 		IndexReader reader = IndexReader.open(directory);
-		assertEquals(ids.length, reader.maxDoc());
+	    assertEquals(ids.length, reader.maxDoc());
 		assertEquals(ids.length, reader.numDocs());
 		reader.close();
 	}
+
+	public void testDeleteBeforeOptimize() throws IOException {
+		IndexWriter writer = getWriter();
+  		assertEquals(2, writer.numDocs());
+		writer.deleteDocuments(new Term("id", "1"));
+		writer.commit();
+	  	assertTrue(writer.hasDeletions());
+		assertEquals(2, writer.maxDoc());
+	 	assertEquals(1, writer.numDocs());
+	  	writer.close();
+		}
+
+	public void testDeleteAfterOptimize() throws IOException {
+		IndexWriter writer = getWriter();
+		assertEquals(2, writer.numDocs());
+	  	writer.deleteDocuments(new Term("id", "1"));
+		writer.optimize();
+		writer.commit();
+		assertFalse(writer.hasDeletions());
+		assertEquals(1, writer.maxDoc());
+		assertEquals(1, writer.numDocs());
+		writer.close();
+	}
+
+	public void testUpdate() throws IOException {
+		assertEquals(1, getHitCount("city", "Amsterdam"));
+		IndexWriter writer = getWriter();
+		Document doc = new Document();
+		doc.add(new Field("id", "1", Field.Store.YES, Field.Index.NOT_ANALYZED));
+		doc.add(new Field("country", "Netherlands", Field.Store.YES, Field.Index.NO));
+		doc.add(new Field("contents", "Den Haag has a lot of museums", Field.Store.NO, Field.Index.ANALYZED));
+		doc.add(new Field("city", "Den Haag", Field.Store.YES, Field.Index.ANALYZED));
+		writer.updateDocument(new Term("id", "1"), doc);
+		writer.close();
+		assertEquals(0, getHitCount("city", "Amsterdam"));
+		assertEquals(1, getHitCount("city", "Den Haag"));
+		}
+
 }
