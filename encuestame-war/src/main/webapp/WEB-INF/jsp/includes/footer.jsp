adfa063150a2be05bc4f9eebe33f2774d8d7759a
Juan Picado <juan@encuestame.org>
2011-01-30 16:47:58 -0600

    javascript error handler with dialogs, mobile template prototype with dojo, removed xhtml unused files

--- encuestame-war/src/main/webapp/WEB-INF/jsp/includes/footer.jsp
+++ encuestame-war/src/main/webapp/WEB-INF/jsp/includes/footer.jsp
@@ -1,5 +1,5 @@
-<div id="sessionHandler" dojoType="encuestame.org.core.commons.error.ErrorSessionHandler"></div>
-<div id="errorHandler" dojoType="encuestame.org.core.commons.error.ErrorHandler"></div>
-<div id="errorConexionHandler" dojoType="encuestame.org.core.commons.error.ErrorConexionHandler"></div>
+<div id="sessionHandler"></div>
+<div id="errorHandler"></div>
+<div id="errorConexionHandler"></div>
 </body>
 </html>
--- encuestame-war/src/main/webapp/resource/js/encuestame/org/core/commons/error/ErrorHandler.js
+++ encuestame-war/src/main/webapp/resource/js/encuestame/org/core/commons/error/ErrorHandler.js
@@ -2,21 +2,22 @@ dojo.provide("encuestame.org.core.commons.error.ErrorHandler");
 
 dojo.require("dojox.widget.Dialog");
 dojo.require("encuestame.org.core.commons.error.AbstractErrorHandler");
+dojo.require("dijit._Templated");
+dojo.require("dijit._Widget");
 
 dojo.declare(
     "encuestame.org.core.commons.error.ErrorHandler",
-    [encuestame.org.core.commons.error.AbstractErrorHandler],{
+    [dijit._Widget, dijit._Templated],{
 
         widgetsInTemplate: true,
 
+        templatePath: dojo.moduleUrl("encuestame.org.core.commons.error", "template/error.inc"),
+
         type : "Error",
 
-        postMixInProperties: function(){
-
-        },
-
         postCreate: function() {
-
+            console.debug("postCreate");
+            //this.inherited(arguments);
         }
     }
 );
