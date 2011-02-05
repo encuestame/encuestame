920a103805c6658e586aa87851c26c21ec461b2e
Juan Picado <juan@encuestame.org>
2011-02-04 19:38:42 -0600

    Fixed compile errorl

---
+++ encuestame-mvc/src/main/java/org/encuestame/mvc/controller/settings/SettingsJsonController.java
@@ -0,0 +1,63 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2001-2011 encuestame: system online surveys Copyright (C) 2009
+ * encuestame Development Team.
+ * Licensed under the Apache Software License version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to  in writing,  software  distributed
+ * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+ * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+ * specific language governing permissions and limitations under the License.
+ ************************************************************************************
+ */
+package org.encuestame.mvc.controller.settings;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.encuestame.mvc.controller.AbstractJsonController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Settings Controller.
+ * @author Picado, Juan juanATencuestame.org
+ * @since Jan 29, 2011 11:37:56 AM
+ * @version $Id:$
+ */
+@Controller
+public class SettingsJsonController extends AbstractJsonController{
+
+    /**
+     * Log.
+     */
+    private Log log = LogFactory.getLog(this.getClass());
+
+    /**
+     * Account Settings.
+     * @param model
+     * @return
+     */
+    @PreAuthorize("hasRole('ENCUESTAME_OWNER')")
+    @RequestMapping(value = "/api/user/settings/profile/{type}/update.json", method = RequestMethod.GET)
+    public ModelMap upgradeProfile(HttpServletRequest request,
+            @PathVariable String type,
+            HttpServletResponse response) throws JsonGenerationException,
+            JsonMappingException, IOException {
+        try {
+        log.debug("account");
+        return returnData();
+    }
+
+}
