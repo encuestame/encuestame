918683c84cbab6ee6ee637eafec54a7634d2e9c3
Paola Morales <paola@encuestame.org>
2011-07-27 13:24:17 -0600

    Progress in ENCUESTAME-267

---
+++ encuestame-persistence/src/main/java/org/encuestame/persistence/domain/dashboard/Dashboard.java
@@ -0,0 +1,208 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2001-2011 encuestame: system online surveys Copyright (C) 2009
+ * encuestame Development Team.
+ * Licensed under the Apache Software License version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to  in writing,  software  distributed
+ * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+ * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+ * specific language governing permissions and limitations under the License.
+ ************************************************************************************
+ */
+package org.encuestame.persistence.domain.dashboard;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.encuestame.persistence.domain.security.Permission;
+import org.encuestame.persistence.domain.security.UserAccount;
+
+/**
+ * Dashboard domain.
+ *
+ * @author Morales, Diana Paola paolaATencuestame.org
+ * @since July 27, 2011
+ */
+@Entity
+@Table(name = "dashboard")
+public class Dashboard {
+
+    /** Dashboard Id. **/
+    private Long boardId;
+
+    /** Dashboard name. **/
+    private String pageBoardName;
+
+    /** Dashboard description. **/
+    private String description;
+
+    /** Favorite. **/
+    private Boolean favorite;
+
+    /** Page Layout. **/
+    private String pageLayout;
+
+    /** Dashboard sequence. **/
+    private Integer boardSequence;
+
+    /** Favorite counter. **/
+    private Integer favoriteCounter;
+
+    /** {@link UserAccount} **/
+    private UserAccount userBoard;
+
+    private Set<Gadget> widgetDashboard = new HashSet<Gadget>();
+
+    /**
+    * @return the boardId
+    */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "dashboardId", unique = true, nullable = true)
+    public Long getBoardId() {
+        return boardId;
+    }
+
+    /**
+    * @param boardId the boardId to set
+    */
+    public void setBoardId(final Long boardId) {
+        this.boardId = boardId;
+    }
+
+    /**
+    * @return the pageBoardName
+    */
+    @Column(name = "dashboardName", nullable = false)
+    public String getPageBoardName() {
+        return pageBoardName;
+    }
+
+    /**
+    * @param pageBoardName the pageBoardName to set
+    */
+    public void setPageBoardName(final String pageBoardName) {
+        this.pageBoardName = pageBoardName;
+    }
+
+    /**
+    * @return the description
+    */
+    @Column(name = "description", nullable = false)
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+    * @param description the description to set
+    */
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+
+    /**
+    * @return the favorite
+    */
+    @Column(name = "favorite", nullable = false)
+    public Boolean getFavorite() {
+        return favorite;
+    }
+
+    /**
+    * @param favorite the favorite to set
+    */
+    public void setFavorite(final Boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    /**
+    * @return the pageLayout
+    */
+    @Column(name = "layout", nullable = false)
+    public String getPageLayout() {
+        return pageLayout;
+    }
+
+    /**
+    * @param pageLayout the pageLayout to set
+    */
+    public void setPageLayout(final String pageLayout) {
+        this.pageLayout = pageLayout;
+    }
+
+    /**
+    * @return the boardSequence
+    */
+    @Column(name = "sequence", nullable = false)
+    public Integer getBoardSequence() {
+        return boardSequence;
+    }
+
+    /**
+    * @param boardSequence the boardSequence to set
+    */
+    public void setBoardSequence(final Integer boardSequence) {
+        this.boardSequence = boardSequence;
+    }
+
+    /**
+    * @return the favoriteCounter
+    */
+    @Column(name = "favorite_counter", nullable = false)
+    public Integer getFavoriteCounter() {
+        return favoriteCounter;
+    }
+
+    /**
+    * @param favoriteCounter the favoriteCounter to set
+    */
+    public void setFavoriteCounter(final Integer favoriteCounter) {
+        this.favoriteCounter = favoriteCounter;
+    }
+
+    /**
+    * @return the userBoard
+    */
+    @ManyToOne()
+    public UserAccount getUserBoard() {
+        return userBoard;
+    }
+
+    /**
+    * @param userBoard the userBoard to set
+    */
+    public void setUserBoard(final UserAccount userBoard) {
+        this.userBoard = userBoard;
+    }
+
+    /**
+    * @return the widgetDashboard
+    */
+    @ManyToMany(cascade=CascadeType.ALL)
+    @JoinTable(name="dashboard_gadget",
+               joinColumns={@JoinColumn(name="dashboardId")},
+               inverseJoinColumns={@JoinColumn(name="widgetId")})
+    public Set<Gadget> getWidgetDashboard() {
+        return widgetDashboard;
+    }
+
+    /**
+    * @param widgetDashboard the widgetDashboard to set
+    */
+    public void setWidgetDashboard(Set<Gadget> widgetDashboard) {
+        this.widgetDashboard = widgetDashboard;
+    }
+}
---
+++ encuestame-persistence/src/main/java/org/encuestame/persistence/domain/dashboard/Gadget.java
@@ -0,0 +1,85 @@
+/*
+ ************************************************************************************
+ * Copyright (C) 2001-2011 encuestame: system online surveys Copyright (C) 2009
+ * encuestame Development Team.
+ * Licensed under the Apache Software License version 2.0
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to  in writing,  software  distributed
+ * under the License is distributed  on  an  "AS IS"  BASIS,  WITHOUT  WARRANTIES  OR
+ * CONDITIONS OF ANY KIND, either  express  or  implied.  See  the  License  for  the
+ * specific language governing permissions and limitations under the License.
+ ************************************************************************************
+ */
+package org.encuestame.persistence.domain.dashboard;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Widget domain.
+ * @author Morales, Diana Paola paolaATencuestame.org
+ * @since July 27, 2011
+ */
+@Entity
+@Table(name = "gadget")
+public class Gadget {
+
+    /** Widget id.**/
+    private Long gadgetId;
+
+    /** Widget name. **/
+    private String gadgetName;
+
+    /** Widget type**/
+    private String gadgetType;
+
+    /**
+    * @return the widgetId
+    */
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "gadgetId", unique = true, nullable = true)
+    public Long getWidgetId() {
+        return gadgetId;
+    }
+
+    /**
+    * @param widgetId the widgetId to set
+    */
+    public void setGadgetId(final Long gadgetId) {
+        this.gadgetId = gadgetId;
+    }
+
+    /**
+    * @return the widgetName
+    */
+    @Column(name = "widgetName", nullable = false)
+    public String getGadgetName() {
+        return gadgetName;
+    }
+
+    /**
+    * @param widgetName the widgetName to set
+    */
+    public void setGadgetName(final String gadgetName) {
+        this.gadgetName = gadgetName;
+    }
+
+    /**
+    * @return the widgetType
+    */
+    public String getGadgetType() {
+        return gadgetType;
+    }
+
+    /**
+    * @param widgetType the widgetType to set
+    */
+    public void setGadgetType(final String gadgetType) {
+        this.gadgetType = gadgetType;
+    }
+}
