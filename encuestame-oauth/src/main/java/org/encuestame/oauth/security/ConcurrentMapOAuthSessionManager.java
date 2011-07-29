0eb0671785146ddc33a0d5ebdb98691ba8ecde28
Juan Picado <juan@encuestame.org>
2011-07-29 13:42:08 +0200

    Moved OAuth support to new module

--- encuestame-oauth/src/main/java/org/encuestame/oauth/security/ConcurrentMapOAuthSessionManager.java
+++ encuestame-oauth/src/main/java/org/encuestame/oauth/security/ConcurrentMapOAuthSessionManager.java
@@ -10,7 +10,7 @@
  * specific language governing permissions and limitations under the License.
  ************************************************************************************
  */
-package org.encuestame.business.security.oauth;
+package org.encuestame.oauth.security;
 
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
