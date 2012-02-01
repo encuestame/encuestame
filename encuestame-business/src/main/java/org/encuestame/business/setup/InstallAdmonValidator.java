a3c8a97b167508025043de5d04bacbe4ebd11982
Juan Picado <juan@encuestame.org>
2012-02-01 07:17:55 +0100

    Validation disabled by spring version conflict.

--- encuestame-business/src/main/java/org/encuestame/business/setup/InstallAdmonValidator.java
+++ encuestame-business/src/main/java/org/encuestame/business/setup/InstallAdmonValidator.java
@@ -4,7 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class InstallAdmonValidator {
-        /*
+
+    /*
+     *
+     * TODO: Spring Binding still use old spring version.
+     * https://github.com/SpringSource/spring-webflow/blob/2.3.x/spring-binding/ivy.xml
+     * This validation is disabled because make conflict with Spring Beans 3.1 version.
     public void validateinstallAdmon(AdministratorProfile administratorProfile, ValidationContext context){
         System.out.println("=============== VALIDATE 2=============");
         MessageContext messages = context.getMessageContext();
