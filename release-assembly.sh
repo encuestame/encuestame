11293b2e7a7190825922354293052160da985d02
Picado, Juan Carlos <juanpicado19@gmail.com>
2012-11-01 11:41:15 +0100

    Updated scm source

--- release-assembly.sh
+++ release-assembly.sh
@@ -1,8 +1,7 @@
 # This script create release assembly of encuestame
 mvn clean
-mvn install -Dmaven.test.skip=true -Pdemo
-
-pushd encuestame-assembly
+mvn install -Dmaven.test.skip=true
+pushd enme-assembly
 mvn clean
 mvn -Drelease=true install
 popd
