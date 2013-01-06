051cfdc7838bfb74fb92e414785ff97c2a6a9196
Picado, Juan Carlos <juanpicado19@gmail.com>
2013-01-06 10:41:47 +0100

    Done ENCUESTAME-582

--- enme-war/enme-tomcat-app/jetty.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn -DMAVEN_OPTS="-Xmx1024m -Xms512m" -Djetty.port=8080 jetty:run
--- enme-war/enme-tomcat-app/jrebel-jetty.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export REBEL_HOME=/Applications/ZeroTurnaround/JRebel
-#export MAVEN_OPTS="-noverify -javaagent:$REBEL_HOME/jrebel.jar $MAVEN_OPTS"
-export MAVEN_OPTS="-noverify -Xbootclasspath/p:$REBEL_HOME/jrebel-bootstrap.jar:$REBEL_HOME/jrebel.jar $MAVEN_OPTS"
-mvn $@
