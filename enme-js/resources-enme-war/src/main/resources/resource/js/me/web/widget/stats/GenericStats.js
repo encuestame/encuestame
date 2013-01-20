275c24d8a84c9c371d199b8b5b4ff8dd5591ce9f
Picado, Juan Carlos <juanpicado19@gmail.com>
2013-01-20 17:15:58 +0100

    Stats widget fixed, profile name fixed

--- enme-js/resources-enme-war/src/main/resources/resource/js/me/web/widget/stats/GenericStats.js
+++ enme-js/resources-enme-war/src/main/resources/resource/js/me/web/widget/stats/GenericStats.js
@@ -1,12 +1,47 @@
-define([ "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin",
+/*
+ * Copyright 2013 encuestame
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/***
+ *  @author juanpicado19D0Tgm@ilDOTcom
+ *  @version 1.146
+ *  @module
+ *  @namespace
+ *  @class
+ */
+define([
+    "dojo/_base/declare",
+    "dijit/_WidgetBase",
+     "dijit/_TemplatedMixin",
     "dijit/_WidgetsInTemplateMixin",
     "me/core/main_widgets/EnmeMainLayoutWidget",
-    "me/web/widget/stream/HashTagInfo", "me/core/enme",
+    "me/web/widget/stream/HashTagInfo",
+    "me/core/enme",
     "dojo/text!me/web/widget/stats/templates/stats.html" ], function(
-    declare, _WidgetBase, _TemplatedMixin, _WidgetsInTemplateMixin,
-    main_widget, hashTagInfo, _ENME, template) {
-  return declare([ _WidgetBase, _TemplatedMixin, main_widget,
-      _WidgetsInTemplateMixin ], {
+    declare,
+    _WidgetBase,
+    _TemplatedMixin,
+    _WidgetsInTemplateMixin,
+    main_widget,
+    hashTagInfo,
+    _ENME,
+    template) {
+  return declare([ _WidgetBase,
+      _TemplatedMixin,
+      main_widget,
+      _WidgetsInTemplateMixin], {
 
     // template string.
     templateString : template,
@@ -44,7 +79,7 @@ define([ "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin",
       },
       load = function(data) {
         if ("success" in data) {
-          this._buildStats(data.success.generic);
+          parent._buildStats(data.success.generic);
         }
       };
       this.getURLService().get(this._service, params,  load, null, null);
