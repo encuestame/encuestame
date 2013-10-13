8e2c9ee5703ad0bc06123d3397e7a9863af6b428
Picado, Juan Carlos <juanpicado19@gmail.com>
2013-10-13 11:32:47 +0200

    New subscribe push service ENCUESTAME-631

--- enme-business/src/main/java/org/encuestame/business/cron/SendNotificationsJob.java
+++ enme-business/src/main/java/org/encuestame/business/cron/SendNotificationsJob.java
@@ -12,13 +12,53 @@
  */
 package org.encuestame.business.cron;
 
+import java.util.Random;
+
+import org.apache.commons.lang.math.RandomUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.encuestame.utils.json.NotificationResume;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+
 /**
  * Publish notifications to users subscribed 
  * @author Picado, Juan juanATencuestame.org
  * @since October 6, 2013
  */
 public class SendNotificationsJob {
-
 	
-	public void sendNotifications(){}
+	private static final Log logger = LogFactory.getLog(SendNotificationsJob.class);
+	
+	@Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+	
+	/**
+	 * @param messagingTemplate the messagingTemplate to set
+	 */
+	public void setMessagingTemplate(final SimpMessageSendingOperations messagingTemplate) {
+		this.messagingTemplate = messagingTemplate;
+	}
+	
+	/**
+	 * 
+	 */
+	public void sendNotifications() {
+		String username = "demo10";
+		final NotificationResume notificationResume = new NotificationResume();
+		notificationResume.setTotalNewNot(RandomUtils.nextLong());
+		notificationResume.setTotalNot(RandomUtils.nextLong());
+		logger.debug("sendNotifications update: " + notificationResume.toString());
+		// en vez del username, un key que se genera en cada login, queda guardado en la bd y se refresca cada x minss
+		this.messagingTemplate.convertAndSend("/topic/notification-updates." + username, notificationResume);
+	}
+
+	/**
+	 * @return the messagingTemplate
+	 */
+	public SimpMessageSendingOperations getMessagingTemplate() {
+		return messagingTemplate;
+	}
+	
+	
 }
