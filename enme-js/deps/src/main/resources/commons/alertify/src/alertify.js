a13dc9ebf5338833edfd34998145708d20cf8f9c
Picado, Juan Carlos <juanpicado19@gmail.com>
2013-10-13 22:25:46 +0200

    Alertify as non git submodule

---
+++ enme-js/deps/src/main/resources/commons/alertify/src/alertify.js
@@ -0,0 +1,9 @@
+define(["proto"], function (AlertifyProto) {
+    "use strict";
+
+    var Alertify = function () {};
+    Alertify.prototype = AlertifyProto;
+    Alertify = new Alertify();
+
+    return Alertify;
+});
