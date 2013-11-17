cf4efacc5fa83ac1939ac59724771b40a185f792
Picado, Juan Carlos <juanpicado19@gmail.com>
2013-11-17 22:38:11 +0100

    Added dojoPanelMenu prototype New mobile js cofiguration Font resources Updated wro4j conf

--- enme-js/resources-enme-war/src/main/resources/resource/js/panel/run.js
+++ enme-js/resources-enme-war/src/main/resources/resource/js/panel/run.js
@@ -10,47 +10,31 @@ require({
   // The base path for all packages and modules. If you don't provide this, baseUrl defaults to the directory
   // that contains dojo.js. Since all packages are in the root, we just leave it blank. (If you change this, you
   // will also need to update `app.profile.js`).
-  //baseUrl: 'resources/js/',
+  baseUrl: '',
 
   // A list of packages to register. Strictly speaking, you do not need to register any packages,
   // but you can't require "app" and get app/main.js if you do not register the "app" package (the loader will look
   // for a module at <baseUrl>/app.js instead). Unregistered packages also cannot use the `map` feature, which
   // might be important to you if you need to relocate dependencies. TL;DR, register all your packages all the time:
   // it will make your life easier.
-
   packages: [
     // If you are registering a package that has an identical name and location, you can just pass a string
     // instead, and it will configure it using that string for both the "name" and "location" properties. Handy!
     'dojo',
-    'dijit',
-    'dojox',
-    {
-      name: 'me',
-      location: 'me',
-      main: "main",
-      map: {}
+    //'dijit',
+    //'dojox',
+    { 
+      name: 'hammer', 
+      location: 'hammer', 
+      main: 'hammer' 
     },
-    {
-      name: 'hammer',
-      location: 'hammer',
-      main: "hammer"
-    },
-    {
-      name: 'panel',
-      location: 'panel',
-      main: "main",
-    },
-    {
-      name: 'mobile',
-      location: 'mobile',
-      main: "main",
-      map: {}
-    },
-    {
-      name: 'chart',
-      location: 'chart',
-      map: {}
+
+    // For reference, this is what a more verbose package declaration looks like.
+    { 
+      name: 'panel', 
+      location: 'panel', 
+      map: {} 
     }
   ]
-// Require `mobile`. This loads the main application module, `mobile/main`, since we registered the `mobile` package above.
-}, [ 'mobile' ]);
+// Require `app`. This loads the main application module, `app/main`, since we registered the `app` package above.
+}, [ 'panel' ]);
