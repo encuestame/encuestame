345a8b8561bd884dd7ef72eea85040689b756cdd
Juan Carlos Picado <juan@encuestame.org>
2014-03-09 02:17:04 +0100

    Fixed ENCUESTAME-597 Updated sql files

--- release-assembly.sh
+++ release-assembly.sh
@@ -1,6 +1,6 @@
 # This script create release assembly of encuestame
 mvn clean
-mvn install -Pproduction -Dmaven.test.skip=true
+mvn install -Pproduction
 pushd enme-assembly
 mvn clean
 mvn -Drelease=true install
