8e932ccb33f180a9d66325a345fcdc74fb1b1f93
Juan Carlos Picado <juan@encuestame.org>
2014-04-30 21:45:22 +0200

    Removed unused css Removed AMD code, not necessary and also create conflicts with Intern

--- enme-js/deps/src/main/resources/commons/sockjs.js
+++ enme-js/deps/src/main/resources/commons/sockjs.js
@@ -2370,9 +2370,9 @@ SockJS.getIframeTransport = function(){
 if ('_sockjs_onload' in window) setTimeout(_sockjs_onload, 1);
 
 // AMD compliance
-if (typeof define === 'function' && define.amd) {
-    define('sockjs', [], function(){return SockJS;});
-}
+//if (typeof define === 'function' && define.amd) {
+//    define('sockjs', [], function(){return SockJS;});
+//}
 //     [*] End of lib/index.js
 
 // [*] End of lib/all.js
