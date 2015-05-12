489c4e411c3bacb34bdd1664c9ad2f5e529125bd
Diana Morales <dianmorales@gmail.com>
2015-05-12 14:45:45 +0200

    Changed API Documentation (Links)

--- enme-mvc/json/json-api1/src/main/java/org/encuestame/mvc/controller/json/v1/LinksJsonController.java
+++ enme-mvc/json/json-api1/src/main/java/org/encuestame/mvc/controller/json/v1/LinksJsonController.java
@@ -52,13 +52,29 @@ public class LinksJsonController extends AbstractJsonControllerV1{
      * @apiName GetSocialLinks
      * @apiGroup Links
      * @apiDescription Retrieve the social links where items has been published.
-     * @apiParam {String} id - XXXX
-     * @apiParam {String} type - XXXX
-     * @apiParam {Number} [start - XXXX
-     * @apiParam {Number} [max - XXXX
+     * @apiParam {String} id Tweetpoll, poll or Survey Id.
+     * @apiParam {String="tweetpoll","poll","survey","profile","hashtag"} type Options available to retrieve all items published.
+     * @apiParam {Number} [start] The minimum number of comments to show in the response.
+     * @apiParam {Number} [max] The maximum number of comments to include in the response.
      * @apiVersion 1.0.0
      * @apiSampleRequest http://www.encuestame.org/demo/api/public/social/links/published.json
      * @apiPermission none
+     * @apiSuccessExample
+     * 	{
+		  "error": {
+
+		  },
+		  "success": {
+		    "links": [
+		      {
+		        "link_url": "https:\/\/twitter.com\/#!\/demo10\/status\/??????",
+		        "provider_social": "TWITTER",
+		        "publishd_text": "What types of books magazines newspapers do you read?",
+		        "published_date": "2014-08-12"
+		      }
+		    ]
+		  }
+		}
      */
     @RequestMapping(value = "/api/public/social/links/published.json", method = RequestMethod.GET)
     public @ResponseBody ModelMap getPublishedSocialLinks(
