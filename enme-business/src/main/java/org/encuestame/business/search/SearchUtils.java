09694593ed84278f71a9bd2ed08bab850748d832
Diana Paola Morales <dianmorales@gmail.com>
2015-09-11 21:50:40 +0200

    Apache-Poi references removed

--- enme-business/src/main/java/org/encuestame/business/search/SearchUtils.java
+++ enme-business/src/main/java/org/encuestame/business/search/SearchUtils.java
@@ -30,22 +30,7 @@ import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.LockObtainFailedException;
-import org.apache.lucene.util.Version;
-import org.apache.pdfbox.cos.COSDocument;
-import org.apache.pdfbox.pdfparser.PDFParser;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.util.PDFTextStripper;
-import org.apache.poi.POIXMLException;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRichTextString;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.lucene.util.Version; 
 import org.encuestame.core.search.DirectoryIndexStore;
 import org.springframework.util.Assert;
 
@@ -84,19 +69,19 @@ public class SearchUtils {
        log.debug("Ext file " + ext);
        return ext;
    }
-
-   /**
-    * PDF Document content parser.
-    * @param is Document content
-    * @return
-    * @throws IOException
-    */
-    public static COSDocument parseDocument(final InputStream is) throws IOException {
-       PDFParser parser = null;
-       parser = new PDFParser(is);
-       parser.parse();
-       return parser.getDocument();
-   }
+//
+//   /**
+//    * PDF Document content parser.
+//    * @param is Document content
+//    * @return
+//    * @throws IOException
+//    */
+//    public static COSDocument parseDocument(final InputStream is) throws IOException {
+//       PDFParser parser = null;
+//       parser = new PDFParser(is);
+//       parser.parse();
+//       return parser.getDocument();
+//   }
 
     /**
      * Add Lucene Document fields.
@@ -126,28 +111,28 @@ public class SearchUtils {
     * @return {@link Document}
     * @throws Exception
     */
-    public static Document createPdfDocument(final File file) throws Exception {
-       InputStream is = new FileInputStream(file);
-       COSDocument cosDoc = null;
-       String docText = "";
-       PDDocument pdDoc = null;
-       try {
-           cosDoc = parseDocument(is);
-           pdDoc = new PDDocument(cosDoc);
-           PDFTextStripper stripper = new PDFTextStripper();
-           docText = stripper.getText(pdDoc);
-           log.debug("PDF Doc Text "+docText.length());
-       }
-       finally {
-            if( pdDoc == null ) {
-                log.error("PdDocument is null");
-            } else {
-                pdDoc.close();
-            }
-       }
-       final Document doc = SearchUtils.addFields(file, docText);
-       return doc;
-   }
+//    public static Document createPdfDocument(final File file) throws Exception {
+//       InputStream is = new FileInputStream(file);
+//       COSDocument cosDoc = null;
+//       String docText = "";
+//       PDDocument pdDoc = null;
+//       try {
+//           cosDoc = parseDocument(is);
+//           pdDoc = new PDDocument(cosDoc);
+//           PDFTextStripper stripper = new PDFTextStripper();
+//           docText = stripper.getText(pdDoc);
+//           log.debug("PDF Doc Text "+docText.length());
+//       }
+//       finally {
+//            if( pdDoc == null ) {
+//                log.error("PdDocument is null");
+//            } else {
+//                pdDoc.close();
+//            }
+//       }
+//       final Document doc = SearchUtils.addFields(file, docText);
+//       return doc;
+//   }
 
     /**
     * Create Document Word.
@@ -157,20 +142,20 @@ public class SearchUtils {
     * @throws POIXMLException
     * @throws Exception
     */
-    public static Document createWordDocument(final File file) throws POIXMLException,
-           Exception {
-       InputStream is = new FileInputStream(file);
-       String bodyText = null;
-       try {
-           XWPFDocument wd = new XWPFDocument(is);
-           XWPFWordExtractor wde = new XWPFWordExtractor(wd);
-           bodyText = wde.getText();
-       } catch (Exception e) {
-           log.debug(e);
-       }
-       Document doc = SearchUtils.addFields(file, bodyText);
-       return doc;
-   }
+//    public static Document createWordDocument(final File file) throws POIXMLException,
+//           Exception {
+//       InputStream is = new FileInputStream(file);
+//       String bodyText = null;
+//       try {
+//           XWPFDocument wd = new XWPFDocument(is);
+//           XWPFWordExtractor wde = new XWPFWordExtractor(wd);
+//           bodyText = wde.getText();
+//       } catch (Exception e) {
+//           log.debug(e);
+//       }
+//       Document doc = SearchUtils.addFields(file, bodyText);
+//       return doc;
+//   }
 
     /**
     * Create Spreadsheets Document.
@@ -179,53 +164,53 @@ public class SearchUtils {
     * @return {@link Document}
     * @throws FileNotFoundException
     */
-    public static Document createSpreadsheetsDocument(final File file) throws Exception {
-       InputStream is = new FileInputStream(file);
-       StringBuilder contents = new StringBuilder();
-       POIFSFileSystem fileSystem = new POIFSFileSystem(is);
-       HSSFWorkbook workBook = new HSSFWorkbook(fileSystem);
-       for (int i = 0; i < workBook.getNumberOfSheets(); i++) {
-           HSSFSheet sheet = workBook.getSheetAt(i);
-           Iterator<Row> rows = sheet.rowIterator();
-           while (rows.hasNext()) {
-               HSSFRow row = (HSSFRow) rows.next();
-               // Display the row number
-               log.debug(row.getRowNum());
-               Iterator<Cell> cells = row.cellIterator();
-               while (cells.hasNext()) {
-                   HSSFCell cell = (HSSFCell) cells.next();
-                   // Display the cell number of the current Row
-                   switch (cell.getCellType()) {
-                   case HSSFCell.CELL_TYPE_NUMERIC: {
-                       log.debug(String.valueOf(cell
-                               .getNumericCellValue()));
-                       contents.append(
-                               String.valueOf(cell.getNumericCellValue()))
-                               .append(" ");
-                       break;
-                   }
-
-                   case HSSFCell.CELL_TYPE_STRING: {
-                       HSSFRichTextString richTextString = cell
-                               .getRichStringCellValue();
-                       log.debug(richTextString.toString());
-                       contents.append(richTextString.toString()).append(" ");
-                       break;
-                   }
-
-                   case HSSFCell.CELL_TYPE_BOOLEAN: {
-                       contents.append(
-                               String.valueOf(cell.getBooleanCellValue()))
-                               .append(" ");
-                       break;
-                   }
-                   }
-               }
-           }
-       }
-       Document doc = SearchUtils.addFields(file, contents.toString());
-       return doc;
-   }
+//    public static Document createSpreadsheetsDocument(final File file) throws Exception {
+//       InputStream is = new FileInputStream(file);
+//       StringBuilder contents = new StringBuilder();
+//       POIFSFileSystem fileSystem = new POIFSFileSystem(is);
+//       HSSFWorkbook workBook = new HSSFWorkbook(fileSystem);
+//       for (int i = 0; i < workBook.getNumberOfSheets(); i++) {
+//           HSSFSheet sheet = workBook.getSheetAt(i);
+//           Iterator<Row> rows = sheet.rowIterator();
+//           while (rows.hasNext()) {
+//               HSSFRow row = (HSSFRow) rows.next();
+//               // Display the row number
+//               log.debug(row.getRowNum());
+//               Iterator<Cell> cells = row.cellIterator();
+//               while (cells.hasNext()) {
+//                   HSSFCell cell = (HSSFCell) cells.next();
+//                   // Display the cell number of the current Row
+//                   switch (cell.getCellType()) {
+//                   case HSSFCell.CELL_TYPE_NUMERIC: {
+//                       log.debug(String.valueOf(cell
+//                               .getNumericCellValue()));
+//                       contents.append(
+//                               String.valueOf(cell.getNumericCellValue()))
+//                               .append(" ");
+//                       break;
+//                   }
+//
+//                   case HSSFCell.CELL_TYPE_STRING: {
+//                       HSSFRichTextString richTextString = cell
+//                               .getRichStringCellValue();
+//                       log.debug(richTextString.toString());
+//                       contents.append(richTextString.toString()).append(" ");
+//                       break;
+//                   }
+//
+//                   case HSSFCell.CELL_TYPE_BOOLEAN: {
+//                       contents.append(
+//                               String.valueOf(cell.getBooleanCellValue()))
+//                               .append(" ");
+//                       break;
+//                   }
+//                   }
+//               }
+//           }
+//       }
+//       Document doc = SearchUtils.addFields(file, contents.toString());
+//       return doc;
+//   }
 
     /**
     * Create Text Document.
