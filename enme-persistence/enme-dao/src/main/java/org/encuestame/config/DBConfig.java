e35f1614107caa777a5acd2f7abc7e90f0503635
Juan Picado (@jotadeveloper) <juanpicado19@gmail.com>
2015-09-23 19:37:43 +0200

    Solved deprecation warning HSEARCH000075: Configuration setting hibernate.search.lucene_version was not specified: using LUCENE_CURRENT.

--- enme-persistence/enme-dao/src/main/java/org/encuestame/config/DBConfig.java
+++ enme-persistence/enme-dao/src/main/java/org/encuestame/config/DBConfig.java
@@ -116,6 +116,7 @@ public class DBConfig {
                 setProperty("hibernate.cache.provider_class", "org.hibernate.cache.NoCacheProvider");
                 setProperty("hibernate.search.default.directory_provider", "org.hibernate.search.store.impl.FSDirectoryProvider");
                 setProperty("hibernate.search.default.indexBase", "/tmp");
+                setProperty("hibernate.search.lucene_version", "LUCENE_CURRENT");
             }
         };
     }
